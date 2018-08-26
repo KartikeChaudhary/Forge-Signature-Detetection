6fe7190d0616507dd20362622ec5b626be9bdb1a
Kartike Chaudhary <42712788+KartikeChaudhary@users.noreply.github.com>
2018-08-26 23:14:28 +0530

    Update decisionTree.R

--- decisionTree.R
+++ decisionTree.R
@@ -1,28 +1,3 @@
-###############################################################
-#                                                             #
-#            Decision Tree for Classification                 #
-#                                                             #
-###############################################################
-#                                                             #
-# Credit: Dr. Prashant Singh Rana                             #
-# Email : psrana@gmail.com                                    #
-# Web   : www.psrana.com                                      #
-#                                                             #
-###############################################################
-#                                                             #
-# Train and Test Decision Tree model for Classification       #
-#                                                             #
-# This script do the following:                               #
-# 1. Load the Data                                            #
-# 2. Partition the data into Train/Test set                   #
-# 3. Train the Decision Tree Model                            #
-# 4. Test                                                     #
-# 5. Evaluate on : Accuracy.                                  # 
-# 6. Finally Saving the results.                              #
-#                                                             #
-###############################################################
-
-
 #--------------------------------------------------------------
 # Step 0: Start; Getting the starting time
 #--------------------------------------------------------------
